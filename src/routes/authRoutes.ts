18db77847dcd195a3e7c2e65145f3a2adb8d43b1
sosse-dev <sosse3135@gmail.com>
2025-08-12 11:50:05 +0800

    feat: add complete auth routes (register, login, profile, etc)

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -5,15 +5,19 @@ import {
   forgotPassword,
   logout,
   getProfile,
+  resetPassword,
 } from "../controller/authController";
 import { authMiddleware } from "../middleware/authMidlleware";
 
 const router = express.Router();
 
+// router yang pakai authMiddleware adalah router yang hanya bisa diakses yang udah punya token JWT di Client alias yang udah login
+// Jika belum login, maka tidak bisa, akan diblok sama authMiddleware
 router.post("/register", register);
 router.post("/login", login);
-router.post("/forgot-password", forgotPassword);
-router.get("/profile", getProfile);
 router.post("/logout", authMiddleware, logout);
+router.get("/profile", authMiddleware, getProfile);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password", resetPassword);
 
 export default router;
