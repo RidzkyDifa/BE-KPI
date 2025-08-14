3f1ecdee124ca8d01f51a28f9695a1bf58263b61
sosse-dev <sosse3135@gmail.com>
2025-08-14 10:27:43 +0800

    feat: updated auth system

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,16 +1,27 @@
 import express from "express";
-import { forgotPassword, getProfile, login, logout, register, resetPassword } from "../controllers/authController";
-import { authMiddleware } from "../middleware/authMiddleware";
 
+import {
+  register,
+  login,
+  forgotPassword,
+  logout,
+  getProfile,
+  resetPassword,
+  verifyEmail,
+  resendVerification,
+} from "../controllers/authController";
+import { authMiddleware } from "../middleware/authMiddleware";
 
 const router = express.Router();
 
-// router yang pakai authMiddleware adalah router yang hanya bisa diakses yang udah punya token JWT di Client alias yang udah login
-// Jika belum login, maka tidak bisa, akan diblok sama authMiddleware
+// sosse (8/12): router yang pakai authMiddleware adalah router yang hanya bisa diakses yang udah punya token JWT di Client alias yang udah login
+// sosse (8/12) Jika belum login, maka tidak bisa, akan diblok sama authMiddleware
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", authMiddleware, logout);
 router.get("/profile", authMiddleware, getProfile);
+router.get("/verify-email/:token", verifyEmail);
+router.post("/resend-verification", resendVerification);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password", resetPassword);
 
