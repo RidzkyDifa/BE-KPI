aecb932a6d36070549f8e581dd29378137e23191
sosse-dev <sosse3135@gmail.com>
2025-08-22 14:55:48 +0800

    feat: add new employee initial route

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes";
+import employeeRoutes from "./routes/employeeRoutes"
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -9,7 +10,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", authRoutes);
+app.use("/api/auth", authRoutes); // Autentikasi User
+app.use("/api/employees", employeeRoutes); // CRUD Karyawan
+
+// Master data routes ( Belum dibuat )
+// app.use("/api/divisions", divisionRoutes);
+// app.use("/api/positions", positionRoutes);
+// app.use("/api/kpis", kpiRoutes);
+
+// Assessment routes (penilaian KPI - Belum dibuat)
+// app.use("/api/assessments", assessmentRoutes);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server running on http://localhost:${process.env.PORT || 3000}`);
